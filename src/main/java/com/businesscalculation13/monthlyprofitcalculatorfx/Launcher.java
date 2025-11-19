fff8e6a688ee20c5d034b0e85a1060e566990c3b
Paul Pasumala <pasumalapaul384@gmail.com>
2025-11-19 19:57:10 +0800

    Delete src/main/java/com/businesscalculation13/monthlyprofitcalculatorfx/Launcher.java

--- src/main/java/com/businesscalculation13/monthlyprofitcalculatorfx/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.businesscalculation13.monthlyprofitcalculatorfx;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // Points to the new Retro Design main class
-        MonthlyProfitCalculatorFX.main(args);
-    }
-}
